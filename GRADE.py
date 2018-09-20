4bf4e2892ba275dbe6870ef88ed9344c86c2bc11
Trishita Gharai <43023150+CMRD2@users.noreply.github.com>
2018-09-20 08:53:31 +0530

    Add files via upload

---
+++ GRADE.py
@@ -0,0 +1,17 @@
+a=int(input("enter the marks"))
+if(a>=90):
+	print("GRADE-A+")
+elif(a>=80):
+	print("GRADE-A")
+elif(a>=70):
+	print("GRADE-B+")
+elif(a>=60):
+	print("GRADE-B")
+elif(a>=50):
+	print("GRADE-C+")
+elif(a>=40):
+	print("GRADE-C")
+elif(a>=35):
+	print("GRADE-D")
+else:
+	print("FAILED")
