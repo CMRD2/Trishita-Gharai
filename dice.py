a9a9436b2d19e02743aaab9f6411713078bc8455
Trishita Gharai <43023150+CMRD2@users.noreply.github.com>
2018-09-25 11:36:01 +0530

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,9 @@
+while True:
+	a=(input("press y to roll the dice-"))
+	if(a=="y"):
+		import random
+		r=random.randint(1,6)
+		print(r)
+	else:
+		print("tata, bye bye")
+		break
---
+++ temp.py
@@ -0,0 +1,4 @@
+print("hello, todays temperature in bangalore is-")
+import random
+r=random.randint(25,37)
+print(r,"'C")
