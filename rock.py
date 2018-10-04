4b55493315ef7fd9efad5f96566523fc84b133a5
Trishita Gharai <43023150+CMRD2@users.noreply.github.com>
2018-10-04 10:25:17 +0530

    Add files via upload

---
+++ rock.py
@@ -0,0 +1,49 @@
+print("hi...! lets play rock paper scissors :D")
+s1=0
+s2=0
+while(s1<=3 or s2<=3):
+	a=input('enter choice(rock-r,paper-p,scissor-s)-')
+	import random
+	d=['r','p','s']
+	s=random.choice(d)
+	print(s)
+	if(a==s):
+		print('draw...')
+		print(s1)
+		print(s2)
+	elif(s1==3):
+		print('you win')
+		break
+	elif(s2==3):
+		print('you lose')
+		break
+	elif(a=='p' and s=='r'):
+		print('point')
+		s1=s1+1
+		print(s1)
+		print(s2)
+	elif(a=='p' and s=='s'):
+		print('try')
+		s2=s2+1
+		print(s1)
+		print(s2)
+	elif(a=='s' and s=='r'):
+		print('try')
+		s2=s2+1
+		print(s1)
+		print(s2)
+	elif(a=='s' and s=='p'):
+		print('point')
+		s1=s1+1
+		print(s1)
+		print(s2)
+	elif(a=='r' and s=='s'):
+		print('point')
+		s1=s1+1
+		print(s1)
+		print(s2)
+	elif(a=='r' and s=='p'):
+		print('try')
+		s2=s2+1
+		print(s1)
+		print(s2)
